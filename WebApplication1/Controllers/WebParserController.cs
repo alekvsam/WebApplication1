a6f1fd469c3a95cc96e3cccd3b8c150318c5c875
Алебастров Роман <alekvsam@hotmail.com>
2021-03-31 13:58:14 +0400

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/WebParserController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Html.Parser;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{    
+    [ApiController]
+    [Route("[controller]")]
+    public class WebParserController : ControllerBase
+    {
+        private readonly ILogger<WebParserController> _logger;
+        
+        // Constructor
+        public WebParserController(ILogger<WebParserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            CheckForUpdates("https://www.avito.ru/krasnodar/kvartiry/sdam/na_dlitelnyy_srok/2-komnatnye-ASgBAQICAkSSA8gQ8AeQUgFAzAgUkFk?cd=1&pmax=25000");
+            return "Success";
+        }
+
+        private async void CheckForUpdates(string url)
+        {
+            List<dynamic> ads = new List<dynamic>();
+            await GetPageContent(url, ads);
+
+        }
+
+        private async Task<List<dynamic>> GetPageContent(string url, List<dynamic> results)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(url);
+
+            var advertItems = document.QuerySelectorAll("div[data-marker='item']");
+
+            foreach (var item in advertItems)
+            {
+                Advert advert = new Advert();
+
+                int.TryParse(item.GetAttribute("data-item-id"), out int id);
+                advert.Id = id;
+
+                float.TryParse(item.QuerySelector("meta[itemprop='price']").GetAttribute("content"), out float price);
+                advert.Price = price;
+                
+                advert.Url = document.Origin + item.QuerySelector("a[itemprop='url']").GetAttribute("href");
+
+#if DEBUG
+                _logger.LogInformation($"the id of ads is {advert.Id}" +
+                    $" and price is {advert.Price}" +
+                    $" and url is {advert.Url}");
+#endif
+
+                results.Add(advert);
+            }
+            return results;
+        }
+    }
+}
---
+++ WebApplication1/Models/Advert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    class Advert
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+        public float Price { get; set; }
+    }
+}
